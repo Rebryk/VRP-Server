3c2f7c331ec7853075bfc9b935cf82ad76370cc4
Sushencev <ism@veeroute.com>
2017-10-28 13:04:31 +0300

    init

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from vrp.google import GRecognizer
 
 logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    with open("config/config.json") as f:
-        config = json.load(f)
+#if __name__ == "__main__":
+with open("config/config.json") as f:
+    config = json.load(f)
 
-    logger.debug("Starting server...")
+logger.debug("Starting server...")
 
-    goolge_asr = GRecognizer()
+goolge_asr = GRecognizer()
 
-    server = Server(asr=goolge_asr)
-    server.start(host=config["host"],
-                 port=config["port"])
+server = Server(asr=goolge_asr, use_cache=False)
+server.start(host=config["host"],
+             port=config["port"])
+
+app = server.app
